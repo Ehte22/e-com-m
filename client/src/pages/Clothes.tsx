7beeb85be243a04d074aaf1d78423de97a7f0f9f
Ehte22 <syedehteshamali22@gmail.com>
2025-04-27 15:06:43 +0530

    Loader added

--- client/src/pages/Clothes.tsx
+++ client/src/pages/Clothes.tsx
@@ -3,16 +3,21 @@ import { useGetProductsQuery } from '../redux/apis/product.api'
 import ProductCard from '../components/ProductCard'
 import { useSearchProduct } from '../context/searchProductContext'
 import { useDebounce } from '../utils/useDebounce'
+import Loader from '../components/Loader'
 
 const Clothes = () => {
     const { searchProduct } = useSearchProduct()
     const debounceSearchQuery = useDebounce(searchProduct, 500)
 
-    const { data } = useGetProductsQuery({
+    const { data, isLoading } = useGetProductsQuery({
         isFetchAll: true,
         searchQuery: debounceSearchQuery.toLowerCase(),
         category: "Clothes"
     })
+
+    if (isLoading) {
+        return <Loader />
+    }
     return <>
         <Box sx={{ mx: { xs: "32px", sm: "8px", md: "40px", lg: "60px" } }}>
             <Grid2 container >
